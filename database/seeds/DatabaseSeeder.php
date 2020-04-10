49dc34e0fa3f5c0083aed2c6da58f98e7c4d79ba
bitiho <biti13012000@gmail.com>
2020-04-10 12:17:40 +0700

    first commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Edit
+        $this->call(UserTableSeeder::class);
+        $this->call(ProductBrandTableSeeder::class);
+        $this->call(ProductTableSeeder::class);
+        $this->call(OrderTableSeeder::class);
+        $this->call(OrderDetailTableSeeder::class);
     }
 }
